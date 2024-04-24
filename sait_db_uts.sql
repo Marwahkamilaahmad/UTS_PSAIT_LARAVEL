f98bb79f017b8235f8d4314c4175aed541ed79af
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-04-24 23:10:40 +0700

    first commit

---
+++ sait_db_uts.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 24 Apr 2024 pada 17.00
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sait_db_uts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(20) DEFAULT NULL,
+  `alamat` varchar(40) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `tanggal_lahir`) VALUES
+('sv_001', 'joko', 'bantul', '0000-00-00'),
+('sv_002', 'paul', 'sleman', '0000-00-00'),
+('sv_003', 'andy', 'surabaya', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kode_mk` varchar(10) NOT NULL,
+  `nama_mk` varchar(20) DEFAULT NULL,
+  `sks` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kode_mk`, `nama_mk`, `sks`) VALUES
+('svpl_001', 'database', 2),
+('svpl_002', 'kecerdasan artifisia', 2),
+('svpl_003', 'interoperabilitas', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `perkuliahan`
+--
+
+CREATE TABLE `perkuliahan` (
+  `id_perkuliahan` int(5) NOT NULL,
+  `nim` varchar(10) DEFAULT NULL,
+  `kode_mk` varchar(10) DEFAULT NULL,
+  `nilai` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `perkuliahan`
+--
+
+INSERT INTO `perkuliahan` (`id_perkuliahan`, `nim`, `kode_mk`, `nilai`) VALUES
+(21, 'sv_003', 'svpl_001', 44);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indeks untuk tabel `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kode_mk`);
+
+--
+-- Indeks untuk tabel `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD PRIMARY KEY (`id_perkuliahan`),
+  ADD KEY `nim` (`nim`),
+  ADD KEY `kode_mk` (`kode_mk`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  MODIFY `id_perkuliahan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD CONSTRAINT `perkuliahan_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`),
+  ADD CONSTRAINT `perkuliahan_ibfk_2` FOREIGN KEY (`kode_mk`) REFERENCES `matakuliah` (`kode_mk`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ sait_project2_uts/mahasiswa_api.php
@@ -0,0 +1,223 @@
+<?php
+require_once "config.php";
+$request_method=$_SERVER["REQUEST_METHOD"];
+switch ($request_method) {
+   case 'GET':
+         if(!empty($_GET["nim"]))
+         {
+            $nim=$_GET["nim"];
+            get_mhs($nim);
+         }
+         else
+         {
+            get_mhss();
+         }
+         break;
+   case 'POST':
+         {
+            insert_mhs();
+         }     
+         break; 
+   case 'DELETE':
+            $nim = $_GET["nim"];
+            $kode_mk = $_GET["kode_mk"];
+            delete_mhs($nim, $kode_mk);
+            break;
+   
+   case 'PUT':
+            break;
+   default:
+
+         header("HTTP/1.0 405 Method Not Allowed");
+         break;
+      break;
+ }
+ 
+   function get_mhss()
+   {
+      global $mysqli;
+      // $query="SELECT * FROM pasiens";
+      $query = "SELECT m.nim, m.nama, p.nilai, mk.nama_mk, p.kode_mk
+      FROM mahasiswa m
+      JOIN perkuliahan p ON m.nim = p.nim
+      JOIN matakuliah mk ON p.kode_mk = mk.kode_mk";
+      $data=array();
+      $result=$mysqli->query($query);
+      while($row=mysqli_fetch_object($result))
+      {
+         $data[]=$row;
+      }
+      $response=array(
+                     'status' => 1,
+                     'message' =>'Get List Mahasiswa Successfully.',
+                     'data' => $data
+                  );
+      header('Content-Type: application/json');
+      echo json_encode($response);
+   }
+
+
+   function get_mhs($nim)
+{
+    global $mysqli;
+    $query = "SELECT m.nim, m.nama, p.nilai, mk.nama_mk, p.kode_mk
+              FROM mahasiswa m
+              JOIN perkuliahan p ON m.nim = p.nim
+              JOIN matakuliah mk ON p.kode_mk = mk.kode_mk";
+    if ($nim != 0) {
+        $query .= " WHERE m.nim = '$nim'";
+    }
+    $data = array();
+    $result = $mysqli->query($query);
+    if ($result) {
+        while ($row = mysqli_fetch_object($result)) {
+            $data[] = $row;
+        }
+        $response = array(
+            'status' => 1,
+            'message' => 'Get Mahasiswa Successfully.',
+            'data' => $data
+        );
+    } else {
+        $response = array(
+            'status' => 0,
+            'message' => 'Failed to get Mahasiswa.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+   
+ 
+   function insert_mhs()
+   {
+       global $mysqli;
+       // Mengecek apakah request berasal dari form atau API
+       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+           $data = $_POST;
+       } else {
+           // Menerima data dari body request untuk API
+           $data = json_decode(file_get_contents('php://input'), true);
+       }
+   
+       // Array untuk memeriksa ketersediaan semua parameter
+       $arrcheckpost = array(
+           'nim' => 'required',
+           'kode_mk' => 'required',
+           'nilai' => 'required',
+       );
+   
+       // Menghitung jumlah parameter yang cocok dengan yang diharapkan
+       $hitung = count(array_intersect_key($data, $arrcheckpost));
+   
+       // Jika jumlah parameter cocok dengan yang diharapkan
+       if ($hitung == count($arrcheckpost)) {
+           // Query untuk memasukkan data ke database
+           $result = mysqli_query($mysqli, "INSERT INTO perkuliahan SET
+                  nim = '$data[nim]',
+                  kode_mk = '$data[kode_mk]', 
+                  nilai = '$data[nilai]'
+                  ");
+           // Memeriksa keberhasilan operasi INSERT
+           if ($result) {
+               $response = array(
+                   'status' => 1,
+                   'message' => 'Nilai Mahasiswa Successfully.'
+               );
+           } else {
+               $response = array(
+                   'status' => 0,
+                   'message' => 'Nilai Mahasiswa Addition Failed.'
+               );
+           }
+       } else {
+           // Jika parameter tnimak sesuai
+           $response = array(
+               'status' => 0,
+               'message' => 'Parameter Do Not Match'
+           );
+       }
+       // Mengatur tipe konten sebagai JSON dan mengirimkan respons
+       header('Content-Type: application/json');
+       echo json_encode($response);
+   }
+
+
+
+ 
+   function update_mhs()
+   // function update_mhs($nim)
+      {
+         global $mysqli;
+         if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+            parse_str(file_get_contents('php://input'), $data);
+            // $data = $_PUT;
+         }else{
+            $data = json_decode(file_get_contents('php://input'), true);
+         }
+
+         // echo json_decode($data);
+         $arrcheckpost = array('nim' => '', 'kode_mk' => '', 'nilai' => '');
+         $hitung = count(array_intersect_key($data, $arrcheckpost));
+
+         $nim = $data['nim'];
+         $kode_mk = $data['kode_mk'];
+         $nilai = $data['nilai'];
+
+         if($hitung == count($arrcheckpost)){
+              $result = mysqli_query($mysqli, "UPDATE perkuliahan SET
+              nilai = '$nilai'
+              WHERE nim ='$nim' AND kode_mk = '$kode_mk'
+              ");
+          
+            if($result)
+            {
+               $response=array(
+                  'status' => 1,
+                  'message' =>'Nilai Mahasiswa Updated Successfully.'
+               );
+            }
+            else
+            {
+               $response=array(
+                  'status' => 0,
+                  'message' =>'Nilai Mahasiswa Updation Failed.'
+               );
+            }
+         }else{
+            $response=array(
+                     'status' => 0,
+                     'message' =>'Nilai Mahasiswa Do Not Match'
+                  );
+         }
+         header('Content-Type: application/json');
+         echo json_encode($response);
+      }
+
+
+
+
+   function delete_mhs($nim, $kode_mk)
+{
+    global $mysqli;
+    $query = "DELETE FROM perkuliahan WHERE nim= '$nim' AND kode_mk = '$kode_mk'";
+    if(mysqli_query($mysqli, $query))
+    {
+        $response = array(
+            'status' => 1,
+            'message' => 'Nilai Mahasiswa Deleted Successfully.'
+        );
+    }
+    else
+    {
+        $response = array(
+            'status' => 0,
+            'message' => 'Nilai Mahasiswa Deletion Failed.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+?> 
